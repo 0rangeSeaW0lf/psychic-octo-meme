da5c191e9712ea54f7cb2d02c4ce32f65b79f227
Jose Moreno <jose.moreno@reyesic.net>
2014-10-09 14:32:07 +0000

    Update bicycle.py

--- bicycle.py
+++ bicycle.py
@@ -1,3 +1,4 @@
+from random import choice
 from frames import Frames
 from wheels import Wheels
 
@@ -7,13 +8,15 @@ class Bicycle(object):
 	    if type(user_input) == type_input:
 	        return user_input
 	
-	def __init__(self):#(self,manufacturer):
+	def __init__(self, manufacturer):
+		# the names list contains model from Specialized bikes as I am a huge fan of its bikes (I own one! :-))
+		names = ["Pitch", "Epic", "Source", "Enduro", "Fatboy", "Status", "Demo", "Tarmac", "Allez", "Venge", "Shiv", "Roubaix", "Secteur", "Diverge", "Awol", "Crux", "Langster", "Sirrus", "Daily", "Crosstail", "CossRoads", "Expedition"]
 		self.frame = Frames()
 		self.wheels = Wheels()
-		#self.manufacturer = manufacturer
-		self.model_name = "Hola"
+		self.manufacturer = manufacturer
+		self.model_name = choice(names)
 		self.weight = (self.frame.frame_weight + 2 * self.wheels.wheel_weight)/1000.0
 		self.cost = self.frame.frame_cost + 2 * self.wheels.wheel_cost
 		
 	def __str__(self):
-	    return "Model: {}\nWeight: {:.2f} Kg\nFrame: {}\nWheels: {}\n".format(self.model_name,self.weight,self.frame.frame_material,self.wheels.wheel_model)
+	    return "Manufacturer: {}\nModel: {}\nWeight: {:.2f} Kg\nFrame: {}\nWheels: {}\n".format(self.manufacturer ,self.model_name,self.weight,self.frame.frame_material,self.wheels.wheel_model)

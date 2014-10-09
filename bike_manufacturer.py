b10c4505957da1d9523689b6e150de1f38de542a
Jose Moreno <jose.moreno@reyesic.net>
2014-10-09 14:50:24 +0000

    Update bike_manufacturer.py

--- bike_manufacturer.py
+++ bike_manufacturer.py
@@ -2,6 +2,20 @@ from bicycle import Bicycle
 
 class Manufacturer(object):
     """docstring for Manufacturer"""
-    def __init__(self, arg):
-        self.arg = arg
-        
+    def make_bikes(self,number,manufacturer):
+        bicycles = []
+        for number in range(number):
+            bicycles.append(Bicycle(manufacturer))
+        return bicycles
+    
+    def __init__(self, manufacturer_name,margin):
+        self.manufacturer_name = manufacturer_name
+        self.margin = margin
+        self.inventory = self.make_bikes(3,self.manufacturer_name)
+        print "Margin: {:.2%}\n".format(self.margin)
+        for bike in self.inventory:
+            print bike
+            print ""
+    
+    def __str__(self):
+        return "{}".format(self.manufacturer_name)

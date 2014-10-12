b42e38e9db7473dcf484673e2c3ae77b44ed33fa
Jose Moreno <jose.moreno@reyesic.net>
2014-10-12 14:15:53 +0000

    Complete Snippet Part 2 - Get a Snippet

--- unit 2/Snippets App/snippets.py
+++ unit 2/Snippets App/snippets.py
@@ -1,4 +1,8 @@
-import csv, logging, argparse, parser, sys
+import csv
+import logging
+import argparse
+import parser
+import sys
 
 logging.basicConfig(filename = "output.log", level = logging.DEBUG)
 
@@ -13,13 +17,27 @@ def put(name, snippet, filename):
     logging.debug("Write sucessful") 
     return name, snippet
     
+def get(name, filename):
+    """Retrieve a snippet with an associated name in the CSV file"""
+    logging.info("Reading {!r} from {!r}".format(name, filename))
+    logging.debug("Opening file")
+    with open(filename, "rb") as f:
+        reader = csv.reader(f,delimiter=',') 
+        logging.debug("Reading snippet to file") 
+        for item in reader:
+            if item[0] == name:
+                logging.debug("Read sucessful") 
+                return item[1]
+        else:
+            return "Snippet not found"
+    
 def make_parser():
     """ Construct the command line parser """ 
     logging.info("Constructing parser")
     description = "Store and retrieve snippets of text" 
     parser = argparse.ArgumentParser(description=description)
     
-    subparsers = parser.add_subparsers(help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
     
     # Subparser for the put command
     logging.debug("Constructing put subparser")
@@ -27,6 +45,13 @@ def make_parser():
     put_parser.add_argument("name", help="The name of the snippet") 
     put_parser.add_argument("snippet", help="The snippet text") 
     put_parser.add_argument("filename", default="snippets.csv", nargs="?",
+help="The snippet filename")
+
+    # Subparser for the put command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet") 
+    get_parser.add_argument("name", help="The name of the snippet")
+    get_parser.add_argument("filename", default="snippets.csv", nargs="?",
 help="The snippet filename")
     
     return parser
@@ -38,9 +63,17 @@ def main():
     arguments = parser.parse_args(sys.argv[1:])
     
     # Convert parsed arguments from Namespace to dictionary arguments = vars(arguments)
+    arguments = vars(arguments)
     command = arguments.pop("command")
+   
     if command == "put":
         name, snippet = put(**arguments)
         print "Stored {!r} as {!r}".format(snippet, name)
+    elif command == "get":
+        name = get(**arguments)
+        if name == "Snippet not found":
+            print "Snippet not found"
+        else:
+            print "Retrieved Snippet {!r}".format(name)
 
 if __name__ == "__main__": main()
